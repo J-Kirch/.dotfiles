663f7193a9696fa7e336a81f15b699ba6bc95ede
JK <jk@jknet.work>
2023-08-26 16:57:56 -0500

    enable saving undotree

--- nvim/.config/nvim/lua/jk/set.lua
+++ nvim/.config/nvim/lua/jk/set.lua
@@ -8,8 +8,9 @@ vim.opt.expandtab = true
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
--- vim.opt.undofile = true
+
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
